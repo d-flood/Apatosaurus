c9e6ba42fa317aaa74777f238b29f4b3194c5426
David Flood <davidfloodii@gmail.com>
2022-11-16 08:57:00 -0600

    setup CustomUser

---
+++ accounts/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
+from accounts import models
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        'email', 
+        'username',
+        'is_staff',
+        'is_superuser',
+    ]
+    # fieldsets = (
+    #     *UserAdmin.fieldsets,  # original form fieldsets, expanded
+    #     (                      
+    #         None,             # group heading of None means we'll have no heading  
+    #         {
+    #             'fields': (),
+    #         },
+    #     ),
+    # )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register((
+    models.UserFeedback,
+    models.BugReport,
+))
---
+++ accounts/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django_registration.forms import RegistrationForm
+
+from accounts import models
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "email",
+            "username",
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "email",
+            "username",
+            "digital_library_access_level",
+        )
+
+
+class CustomUserRegistrationForm(RegistrationForm):
+    # TODO: add a captha field
+    class Meta(RegistrationForm.Meta):
+        model = get_user_model()
+
+
+class UserFeedbackForm(forms.ModelForm):
+    class Meta:
+        model = models.UserFeedback
+        fields = (
+            'message',
+        )
+
+
+class BugReportForm(forms.ModelForm):
+    class Meta:
+        model = models.BugReport
+        fields = (
+            'steps_to_reproduce',
+            'expected_result',
+            'actual_result',
+            'error_text',
+            'comments',
+        )
