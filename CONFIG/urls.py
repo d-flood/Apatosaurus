54229ff7b2d6706929404147ab16bd6ff6b9d0a3
David Flood <davidfloodii@gmail.com>
2022-11-16 11:30:44 -0600

    add routes

--- CONFIG/urls.py
+++ CONFIG/urls.py
@@ -13,9 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import environ
+
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.urls import path
 
+from CONFIG import settings
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'home.html')
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
+    path('', home, name='home'),
 ]
+
+# if settings.DEBUG:
+#     from django.conf.urls.static import static
+#     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#     # Serve static and media files from development server
+#     urlpatterns += staticfiles_urlpatterns()
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
