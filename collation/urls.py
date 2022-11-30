c366588de9a02805cb59c1859313576554418325
David Flood <davidfloodii@gmail.com>
2022-11-30 13:13:02 -0600

    enable editing of rdgs and js niceities

--- collation/urls.py
+++ collation/urls.py
@@ -8,6 +8,7 @@ urls = [
     path('new/section/<int:collation_id>/', views.new_section, name='new-section'),
     path('new/ab/<int:section_id>/', views.new_ab, name='new-ab'),
     path('new/rdg/<int:app_pk>/', views.new_rdg, name='new-rdg'),
+    path('edit-rdg/<int:rdg_pk>/', views.edit_rdg, name='edit-rdg'),
     path('new/rdg-cancel/<int:app_pk>/', views.cancel_new_rdg, name='cancel-new-rdg'),
     path('sections/<int:collation_id>/', views.sections, name='section-list'),
     path('collation-list/', views.collations, name='collation-list'),
