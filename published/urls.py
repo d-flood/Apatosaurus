7266324108a3ef3286ee2afd06618d7bc6047c4f
David Flood <davidfloodii@gmail.com>
2022-12-29 16:41:11 -0600

    wip: create published apparatus view

--- published/urls.py
+++ published/urls.py
@@ -5,6 +5,12 @@ from published import views
 urls = [
     path('', views.main, name='published'),
     path('browse-collation/<int:collation_pk>/', views.browse_collation, name='browse-collation'),
+    path('browse-collation/<int:collation_pk>/<int:user_pk>/', views.browse_collation, name='browse-collation'),
     path('browse-section/<int:section_pk>/', views.browse_section, name='browse-section'),
+    path('browse-section/<int:section_pk>/<int:user_pk>/', views.browse_section, name='browse-section'),
     path('browse-editor/<int:user_pk>/', views.browse_editor, name='browse-editor'),
+    path('apparatus/<int:ab_pk>/', views.apparatus, name='published-apparatus'),
+    path('apparatus/<int:ab_pk>/<int:user_pk>/', views.apparatus, name='published-apparatus'),
+    # path('browse-section-editor/<int:section_pk>/<int:user_pk>/', views.browse_section_editor, name='browse-section-editor'),
+    # path('browse-collation-editor/<int:collation_pk>/<int:user_pk>/', views.browse_collation_editor, name='browse-collation-editor'),
 ]
