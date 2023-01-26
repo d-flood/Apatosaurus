56cfbf30b0ea71f2d4a2df6a9ce3e3bc8fb958f6
David Flood <davidfloodii@gmail.com>
2023-01-26 14:05:58 -0600

    don't poll so fast

--- wait_for_db.py
+++ wait_for_db.py
@@ -5,7 +5,7 @@ def main():
     db_path = Path('db/apatosaurus.db')
     while not db_path.exists():
         print('Waiting for database...')
-        time.sleep(1)
+        time.sleep(2)
     print('Database found!')
 
 if __name__ == '__main__':
