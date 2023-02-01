e1416ddf54b6b55c697e2dd4b282e2c939b8c2ad
David Flood <davidfloodii@gmail.com>
2023-02-01 16:53:30 -0600

    add ImageBlock

--- content/models.py
+++ content/models.py
@@ -22,3 +22,20 @@ class AboutPage(models.Model):
 
     class Meta:
         ordering = ('order',)
+
+class ImageBlock(models.Model):
+    image = models.ImageField(upload_to='content/about/images/', blank=False)
+    markdown = models.TextField(blank=False)
+    html = models.TextField(blank=True)
+    order = models.SmallIntegerField(default=0)
+    about_page = models.ForeignKey(AboutPage, on_delete=models.CASCADE, related_name='blocks')
+
+    def __str__(self):
+        return f'{self.about_page.title} > {self.markdown[:20]}'
+
+    def save(self, *args, **kwargs):
+        self.html = markdown(self.markdown)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('order',)
