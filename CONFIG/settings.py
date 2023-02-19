48eaf6e7572d3aacc19aa6284a7dc75292929645
David Flood <davidfloodii@gmail.com>
2023-02-19 10:24:50 -0600

    move from sqlite to postgres

--- CONFIG/settings.py
+++ CONFIG/settings.py
@@ -101,35 +101,35 @@ WSGI_APPLICATION = 'CONFIG.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db' / 'apatosaurus.db',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db' / 'apatosaurus.db',
+#     }
+# }
 
-# if environ.get('USE_PRODUCTION_DB') != 'True':
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'postgres',
-#             'USER': 'postgres',
-#             'PASSWORD': 'atestpasswordforgettingup',
-#             'HOST': 'db',
-#             'PORT': 5432
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': environ.get('PRODUCTION_POSTGRES_NAME'),
-#             'USER': environ.get('PRODUCTION_POSTGRES_USER'),
-#             'PASSWORD': environ.get('PRODUCTION_POSTGRES_PASSWORD'),
-#             'HOST': environ.get('PRODUCTION_POSTGRES_HOST'),
-#             'PORT': 5432
-#         }
-#     }
+if environ.get('USE_PRODUCTION_DB') != 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'atestpasswordforgettingup',
+            'HOST': 'db',
+            'PORT': 5432
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': environ.get('PRODUCTION_POSTGRES_NAME'),
+            'USER': environ.get('PRODUCTION_POSTGRES_USER'),
+            'PASSWORD': environ.get('PRODUCTION_POSTGRES_PASSWORD'),
+            'HOST': environ.get('PRODUCTION_POSTGRES_HOST'),
+            'PORT': 5432
+        }
+    }
 
 
 CACHE_TTL = 60
