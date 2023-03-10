305bfc60dcdd9126ce9ac39618b19aacda20b43c
David Flood <davidfloodii@gmail.com>
2023-03-10 11:04:58 -0600

    generate graphs for large collations with aws batch

---
+++ cbgm/management/commands/print_global_stemma.py
@@ -0,0 +1,24 @@
+import json
+
+from django.core.management.base import BaseCommand, CommandError
+
+from cbgm.tasks import global_stemma_task
+
+
+
+class Command(BaseCommand):
+    help = 'Calls the open-cbgm interface to generate a global stemma diagram'
+
+    def add_arguments(self, parser):
+        parser.add_argument('job_pk', nargs=1, type=int)
+        parser.add_argument('db_pk', nargs=1, type=int)
+        parser.add_argument('data', nargs=1, type=str)
+
+    def handle(self, *args, **options):
+        job_pk = options['job_pk'][0]
+        db_pk = options['db_pk'][0]
+        data = json.loads(options['data'][0])
+        
+        global_stemma_task(job_pk, db_pk, data)
+           
+        self.stdout.write(self.style.SUCCESS(f'Successfully generated Global Stemma Diagram'))
