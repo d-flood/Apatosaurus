25120efe06438490aca5c93f6f8379e12cbe3222
David Flood <davidfloodii@gmail.com>
2023-11-15 22:18:43 -0600

    create and style "presentation" page

--- content/urls.py
+++ content/urls.py
@@ -7,4 +7,5 @@ from content import views
 urls = [
     path('about/', views.index, name='about'),
     path('about/<slug:slug>/', views.index, name='about'),
+    path('<slug:slug>/', views.index, name='presentation'),
 ]
