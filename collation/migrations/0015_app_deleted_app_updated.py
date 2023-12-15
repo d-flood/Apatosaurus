d63ce18de1fa9cc9a93a22e7f5b777e3ccacb33a
David Flood <davidfloodii@gmail.com>
2023-12-15 17:57:18 -0600

    rename migration

--- collation/migrations/0015_app_deleted_app_updated.py
+++ collation/migrations/0015_app_deleted_app_updated.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('collation', '0013_app_slugname'),
+        ('collation', '0014_ab_right_to_left'),
     ]
 
     operations = [
