6ca36bd49fc6b1012ea68b6c0da052ad6a1d5ee0
David Flood <davidfloodii@gmail.com>
2024-01-09 23:11:09 -0600

    Refactor refresh_liste.py to use Witness.get_by_siglum() method

--- collation/management/commands/refresh_liste.py
+++ collation/management/commands/refresh_liste.py
@@ -1,39 +1,42 @@
+import json
+
 from django.core.management.base import BaseCommand, CommandError
 
 from collation.models import Witness
 
-import json
 
 class Command(BaseCommand):
-    help = 'Refreshes the List of Witnesses from the INTF Liste'
+    help = "Refreshes the List of Witnesses from the INTF Liste"
 
     def add_arguments(self, parser):
-        parser.add_argument('file', nargs=1, type=str)
+        parser.add_argument("file", nargs=1, type=str)
 
     def handle(self, *args, **options):
-        json_file = options['file'][0]
-        
-        with open(json_file, 'r', encoding='utf-8') as f:
+        json_file = options["file"][0]
+
+        with open(json_file, "r", encoding="utf-8") as f:
             data = json.load(f)
-        
-        liste = data['data']['manuscripts']['manuscript']
+
+        liste = data["data"]["manuscripts"]["manuscript"]
         for m in liste:
-            if m['docID'] > 49999:
+            if m["docID"] > 49999:
                 break
-            elif m['docID'] < 20000:
-                if m['gaNum'] in ['', None]:
+            elif m["docID"] < 20000:
+                if m["gaNum"] in ["", None]:
                     continue
-                siglum = m['gaNum']
-            elif m['docID'] < 30000:
+                siglum = m["gaNum"]
+            elif m["docID"] < 30000:
                 siglum = f"0{m['gaNum']}"
-            elif m['docID'] < 40000:
+            elif m["docID"] < 40000:
                 siglum = f"{m['gaNum']}"
             else:
                 siglum = f"{m['gaNum']}"
-            
+
             try:
-                Witness.objects.get(siglum=siglum)
+                Witness.get_by_siglum(siglum)
             except Witness.DoesNotExist:
-                Witness.objects.create(siglum=siglum, description=m.get('orig'), default=True)
-           
-        self.stdout.write(self.style.SUCCESS('Successfully refreshed liste'))
+                Witness.objects.create(
+                    siglum=siglum, description=m.get("orig"), default=True
+                )
+
+        self.stdout.write(self.style.SUCCESS("Successfully refreshed liste"))
