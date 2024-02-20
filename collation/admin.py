3198928c06bf1b8373cb740abe0e933727fe0c06
David Flood <davidfloodii@gmail.com>
2024-02-20 14:12:15 -0600

    add CollationConfig to admin

--- collation/admin.py
+++ collation/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 
 from collation import models
 
-admin.site.register((
-    models.Collation,
-    models.Section,
-    models.Ab,
-    models.App,
-    models.Witness,
-    models.Rdg,
-    models.Arc,
-    models.RdgHistory,
-))
+admin.site.register(
+    (
+        models.Collation,
+        models.Section,
+        models.Ab,
+        models.App,
+        models.Witness,
+        models.Rdg,
+        models.Arc,
+        models.RdgHistory,
+        models.CollationConfig,
+    )
+)
