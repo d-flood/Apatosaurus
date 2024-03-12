3efd9797ece09fe6b1c45de93915985bb5993c15
David Flood <davidfloodii@gmail.com>
2024-03-12 15:02:28 -0500

    load witnesses in stages to avoid lambda 6mb response limit

--- witnesses/views.py
+++ witnesses/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods, require_safe
@@ -11,16 +12,34 @@ from witnesses import forms
 @login_required
 @require_safe
 def main(request: HttpRequest) -> HttpResponse:
-    default_witnesses = cmodels.Witness.objects.filter(default=True)
-    user_witnesses = cmodels.Witness.objects.filter(user=request.user)
-    form = forms.WitnessForm()
+    default_witnesses = cmodels.Witness.objects.filter(default=True).order_by("pk")
+    if request.htmx:
+        user_witnesses = None
+        form = None
+        template = "witnesses.html#default-witnesses"
+        resp_headers = {"Cache-Control": f"private, max-age={60*60}"}
+    else:
+        user_witnesses = cmodels.Witness.objects.filter(user=request.user)
+        form = forms.WitnessForm()
+        template = "witnesses.html"
+        resp_headers = {}
+
+    paginator = Paginator(default_witnesses, 500)
+    page_number = int(request.GET.get("page", 1))
+    default_witnesses_page = paginator.get_page(page_number)
+
     context = {
         "page": {"active": "witnesses", "title": "Apatosaurus - Witnesses"},
-        "default_witnesses": default_witnesses,
+        "default_witnesses": default_witnesses_page,
         "user_witnesses": user_witnesses,
         "form": form,
+        "page_number": page_number,
     }
-    return render(request, "witnesses/main.html", context)
+
+    resp = render(request, template, context)
+    for k, v in resp_headers.items():
+        resp[k] = v
+    return resp
 
 
 @login_required
