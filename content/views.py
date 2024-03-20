f2d396446c59b13688fc1bc88749601eae3d169e
David Flood <davidfloodii@gmail.com>
2024-03-20 07:52:32 -0500

    add (but don't activate) error toast on htmx 400–500

--- content/views.py
+++ content/views.py
@@ -1,33 +1,30 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.http import HttpRequest, HttpResponse, Http404
-from django.shortcuts import get_object_or_404
-from django.views.decorators.http import require_safe
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+from django.views.decorators.http import require_safe
 
 from content.models import AboutPage
 
 
 @require_safe
-def index(request: HttpRequest, slug: str = '') -> HttpResponse:
+def index(request: HttpRequest, slug: str = "") -> HttpResponse:
     if slug:
         about_page = get_object_or_404(AboutPage, slug=slug)
     else:
         if not (about_page := AboutPage.objects.first()):
             raise Http404('The "About" page is missing. That is my problem, not yours.')
-        return redirect(reverse('about', kwargs={'slug': about_page.slug}))
+        return redirect(reverse("about", kwargs={"slug": about_page.slug}))
     context = {
-        'page': {
-            'description': 'A Web Application for visualizing, editing, analyzing, and publishing digital collations of ancient texts.',
-            'active': 'about',
-            'title': f'Apatosaurus - {about_page.title}',
+        "page": {
+            "description": "A Web Application for visualizing, editing, analyzing, and publishing digital collations of ancient texts.",
+            "active": "about",
+            "title": f"Apatosaurus - {about_page.title}",
         },
-        'about_page': about_page,
-        'about_active': slug,
-        'pages': AboutPage.objects.all(),
+        "about_page": about_page,
+        "about_active": slug,
+        "pages": AboutPage.objects.all(),
     }
-    template = 'about.html' if about_page.page_type == 'normal' else 'presentation.html'
+    template = "about.html" if about_page.page_type == "normal" else "presentation.html"
     return render(request, template, context)
-
-
