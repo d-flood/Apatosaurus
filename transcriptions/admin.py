4aef05f45099b26ad575b86e0eb7c4e808c297b1
David Flood <davidfloodii@gmail.com>
2024-04-25 07:34:22 -0500

    enable search and filtering for transcriptions

--- transcriptions/admin.py
+++ transcriptions/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from transcriptions import models
 
-admin.site.register(
-    models.Transcription,
-)
+
+class TranscriptionAdmin(admin.ModelAdmin):
+    search_fields = (
+        "name",
+        "witness__siglum",
+    )
+    list_filter = ("user",)
+
+
+admin.site.register(models.Transcription, TranscriptionAdmin)
