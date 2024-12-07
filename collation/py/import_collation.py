b3a995da2ef20e3a7a1b965aec12e5cc26c1ae6f
David Flood <69060117+d-flood@users.noreply.github.com>
2024-12-06 22:24:17 -0600

    wip: begin adding peasy_jobs

--- collation/py/import_collation.py
+++ collation/py/import_collation.py
@@ -1,56 +1,58 @@
-# # TODO: refactor long running tasks
+# TODO: refactor long running tasks
 
-# import os
+import os
 
-# import boto3
+import boto3
+from peasy_jobs.peasy_jobs import peasy
 
-# from accounts.py.update_status import JobStatus, update_status
-# from collation import models
-# from collation.py import process_tei
-# from CONFIG import settings
-
-
-# def tei_to_db(tei_file_name: str, section_pk: int, user_pk: int):
-#     session = boto3.Session(
-#         aws_access_key_id=os.environ.get("AWS_S3_ACCESS_KEY_ID"),
-#         aws_secret_access_key=os.environ.get("AWS_S3_SECRET_ACCESS_KEY"),
-#         region_name=os.environ.get("AWS_S3_REGION_NAME"),
-#     )
-#     s3 = session.client("s3")
-#     tei_string = (
-#         s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=tei_file_name)[
-#             "Body"
-#         ]
-#         .read()
-#         .decode("utf-8")
-#     )
-#     job_pk = JobStatus.objects.create(
-#         user_id=user_pk,
-#         name=f"Enqueue batch jobs for {models.Section.objects.get(pk=section_pk).name}",
-#         message="",
-#     ).pk
-#     try:
-#         if (xml := process_tei.parse_xml(tei_string)) is not None:
-#             process_tei.tei_to_db(xml, section_pk, user_pk)
-#             update_status(job_pk, "Done", 100, False, True)
-#         else:
-#             update_status(job_pk, "Error: XML file is not valid", 0, False, False, True)
-#     except Exception as e:
-#         print(e)
-#         update_status(job_pk, f"Error: {e}", 0, False, False, True)
-#     s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=tei_file_name)
+from accounts.py.update_status import JobStatus, update_status
+from collation import models
+from collation.py import process_tei
+from CONFIG import settings
 
 
-# def import_tei(tei_file, tei_file_name: str, section_pk: int, user_pk: int):
-#     session = boto3.Session(
-#         aws_access_key_id=os.environ.get("AWS_S3_ACCESS_KEY_ID"),
-#         aws_secret_access_key=os.environ.get("AWS_S3_SECRET_ACCESS_KEY"),
-#         region_name=os.environ.get("AWS_S3_REGION_NAME"),
-#     )
-#     s3 = session.client("s3")
-#     s3.put_object(
-#         Bucket=settings.AWS_STORAGE_BUCKET_NAME,
-#         Key=tei_file_name,
-#         Body=tei_file.read(),
-#     )
-#     tei_to_db(tei_file_name, section_pk, user_pk)
+@peasy.job("import tei")
+def tei_to_db(tei_file_name: str, section_pk: int, user_pk: int):
+    session = boto3.Session(
+        aws_access_key_id=os.environ.get("AWS_S3_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_S3_SECRET_ACCESS_KEY"),
+        region_name=os.environ.get("AWS_S3_REGION_NAME"),
+    )
+    s3 = session.client("s3")
+    tei_string = (
+        s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=tei_file_name)[
+            "Body"
+        ]
+        .read()
+        .decode("utf-8")
+    )
+    job_pk = JobStatus.objects.create(
+        user_id=user_pk,
+        name=f"Enqueue batch jobs for {models.Section.objects.get(pk=section_pk).name}",
+        message="",
+    ).pk
+    try:
+        if (xml := process_tei.parse_xml(tei_string)) is not None:
+            process_tei.tei_to_db(xml, section_pk, user_pk)
+            update_status(job_pk, "Done", 100, False, True)
+        else:
+            update_status(job_pk, "Error: XML file is not valid", 0, False, False, True)
+    except Exception as e:
+        print(e)
+        update_status(job_pk, f"Error: {e}", 0, False, False, True)
+    s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=tei_file_name)
+
+
+def import_tei(tei_file, tei_file_name: str, section_pk: int, user_pk: int):
+    session = boto3.Session(
+        aws_access_key_id=os.environ.get("AWS_S3_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_S3_SECRET_ACCESS_KEY"),
+        region_name=os.environ.get("AWS_S3_REGION_NAME"),
+    )
+    s3 = session.client("s3")
+    s3.put_object(
+        Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+        Key=tei_file_name,
+        Body=tei_file.read(),
+    )
+    tei_to_db(tei_file_name, section_pk, user_pk)
