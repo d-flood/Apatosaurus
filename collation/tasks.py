0ebccf923aaa59c0ef1537d857b4da42f75ec1b3
David Flood <69060117+d-flood@users.noreply.github.com>
2024-12-07 20:31:04 -0600

    refactor to use PeasyJobQueue instead of JobStatus for tracking

--- collation/tasks.py
+++ collation/tasks.py
@@ -5,7 +5,7 @@ import logging
 from django.core.files.base import ContentFile
 from peasy_jobs.peasy_jobs import peasy
 
-from accounts.models import JobStatus, UserFile
+from accounts.models import UserFile
 from collation.models import Collation, Section
 from collation.py import process_tei
 
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 @peasy.job("download apparatus")
-def download_apparatus_task(corpus_type: str, user_pk: int, corpus_pk: int):
+def download_apparatus_task(corpus_type: str, user_pk: int, corpus_pk: int, job_pk: int):
     """Generate a TEI collation file for either a Section or a Collation object."""
     if corpus_type == "section":
         corpus = Section.objects.get(pk=corpus_pk)
@@ -21,76 +21,27 @@ def download_apparatus_task(corpus_type: str, user_pk: int, corpus_pk: int):
         corpus = Collation.objects.get(pk=corpus_pk)
     else:
         raise ValueError("Invalid corpus type.")
-    job_pk = JobStatus.objects.create(
+    peasy.update_status(job_pk, f"Generate TEI for {corpus.name}")
+    tei = corpus.as_tei(job_pk).encode("utf-8")
+    peasy.update_status(job_pk, "TEI generated. Now adding to your files.")
+    
+    UserFile(
         user_id=user_pk,
-        in_progress=True,
-        progress=0,
-        name=f"Generate TEI for {corpus.name}",
-        message="Starting task...",
-    ).pk
-    try:
-        tei = corpus.as_tei(job_pk).encode("utf-8")
-        JobStatus.objects.filter(pk=job_pk).update(
-            progress=100,
-            message="Created TEI, adding to your files...",
-        )
-    except Exception as e:
-        JobStatus.objects.filter(pk=job_pk).update(
-            in_progress=False,
-            failed=True,
-            message=f"Error: {e}",
-        )
-        return
-    try:
-        UserFile(
-            user_id=user_pk,
-            name=f"{corpus.name}.xml",
-            file=ContentFile(tei, name=f"{corpus.name}.xml"),
-        ).save()
-        JobStatus.objects.filter(pk=job_pk).update(
-            in_progress=False,
-            completed=True,
-            message="TEI file added to your files.",
-        )
-    except Exception as e:
-        JobStatus.objects.filter(pk=job_pk).update(
-            in_progress=False,
-            failed=True,
-            message=f"Error: {e}",
-        )
-    return
+        name=f"{corpus.name}.xml",
+        file=ContentFile(tei, name=f"{corpus.name}.xml"),
+    ).save()
+    peasy.update_status(job_pk, "TEI file added to your files.")
+    
 
 @peasy.job("build collation index")
 def build_collation_index(
-    collation_pk: int, user_pk: int, child_rdg_types: list[str] = None
+    collation_pk: int, user_pk: int, child_rdg_types: list[str] = None, job_pk: int = None
 ):
     """Build the index for a collation."""
     collation = Collation.objects.get(pk=collation_pk)
-    job_pk = JobStatus.objects.create(
-        user_id=user_pk,
-        in_progress=True,
-        progress=0,
-        name=f"Build index for {collation.name}",
-        message="Starting task...",
-    ).pk
-    try:
-        JobStatus.objects.filter(pk=job_pk).update(
-            progress=0,
-            message="Building index...",
-        )
-        collation.build_index(child_rdg_types)
-        JobStatus.objects.filter(pk=job_pk).update(
-            progress=100,
-            completed=True,
-            message="Index built.",
-        )
-    except Exception as e:
-        JobStatus.objects.filter(pk=job_pk).update(
-            in_progress=False,
-            failed=True,
-            message=f"Error: {e}",
-        )
-    return
+    peasy.update_status(job_pk, f"Building index for {collation.name}")
+    collation.build_index(child_rdg_types)
+    peasy.update_status(job_pk, f"Index built for {collation.name}")
 
 
 @peasy.job("import tei")
